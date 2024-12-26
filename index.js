04b8f619893924d9f0309482025123f956b2046d
Sinan <nanossn1008@gmail.com>
2024-12-26 01:54:48 +0000

    Finish one round logic

--- index.js
+++ index.js
@@ -12,7 +12,40 @@ function getComputerChoice() {
 function getHumanChoice() {
     let input;
     do {
-        input = prompt("Please enter rock/paper/scissors: ");
+        input = prompt("Please enter rock/paper/scissors: ").toLowerCase();
     } while (input != "rock" && input != "paper" && input != "scissors");
     return input;
+}
+
+let humanScore, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        return "It's a tie!";
+    }
+    if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            computerScore++;
+            return "Computer wins! Paper beats Rock.";
+        } else {
+            humanScore++;
+            return "Human wins! Rock beats Scissors.";
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            humanScore++;
+            return "Human wins! Paper beats Rock.";
+        } else {
+            computerScore++;
+            return "Computer wins! Scissors beats Paper.";
+        }
+    } else {
+        if (computerChoice == "rock") {
+            computerScore++;
+            return "Computer wins! Rock beats Scissors.";
+        } else {
+            humanScore++;
+            return "Human wins! Scissors beats Paper.";
+        }
+    }
 }
