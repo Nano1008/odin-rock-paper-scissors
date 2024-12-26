76953b3f52352033b927965c5bbaa767bd41ca7d
Sinan <nanossn1008@gmail.com>
2024-12-26 00:52:28 +0000

    Finish computer choice

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let randomNum = Math.random();
+    if (randomNum >= 0 && randomNum < 0.3) {
+        return "rock";
+    } else if (randomNum >= 0.3 && randomNum < 0.6) {
+        return "paper";
+    } else {
+        return 'scissors';
+    }
+}
